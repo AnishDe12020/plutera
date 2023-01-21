870274e2f8e4ba84c0bacf830e2e9d4f83a7784b
apoorvcodes <tofu.develops@gmail.com>
2023-01-21 15:18:03 +0530

    feat: user and buidl delete methods

---
+++ pages/api/user/delete.ts
@@ -0,0 +1,21 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { prisma } from '../../../src/lib/db';
+
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<any>
+) {
+    if (req.method === "DELETE") { 
+    const userData = await prisma.user.delete({
+        where: {
+            id: req.body.id
+        }
+    })
+    res.status(200).json({
+        status: 200,
+        data: userData,
+    })
+   }
+}
