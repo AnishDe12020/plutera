b1e5e0b0d6bc9def6e26929e49245a04a5e46b51
apoorvcodes <tofu.develops@gmail.com>
2023-01-21 15:23:54 +0530

    feat: useMint hook

---
+++ src/enums/images.ts
@@ -0,0 +1,7 @@
+export enum Images {
+    zero = 'https://media.discordapp.net/attachments/1052253873001205790/1064578619055284365/DALLE_2023-01-16_21.44.11_-_egg_pixelated_nft_design.png',
+    one = 'https://media.discordapp.net/attachments/1052253873001205790/1064578788723265576/DALLE_2023-01-16_21.44.22_-_baby_dragon_pixelated_nft_design.png',
+    two = 'https://media.discordapp.net/attachments/1052253873001205790/1064578954717057024/DALLE_2023-01-16_21.44.39.png',
+    three = 'https://media.discordapp.net/attachments/1052253873001205790/1064579011952509100/DALLE_2023-01-16_21.44.47.png',
+    four = 'https://media.discordapp.net/attachments/1052253873001205790/1064579069892636782/DALLE_2023-01-16_21.44.55.png'
+  }
