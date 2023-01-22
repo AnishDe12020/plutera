2d47404f3a20c8704781a3e7cced943efe798878
Anish De <63192115+AnishDe12020@users.noreply.github.com>
2023-01-22 14:56:21 +0530

    proposals and updates api routes

---
+++ pages/api/proposals/index.ts
@@ -0,0 +1,84 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { prisma } from "../../../src/lib/db";
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  switch (req.method) {
+    case "GET":
+      await handleGetProposalsForBuidl(req, res);
+      break;
+
+    case "POST":
+      await handleCreateProposal(req, res);
+      break;
+
+    default:
+      return res.status(405).json({ message: "Method not allowed" });
+  }
+};
+
+const handleGetProposalsForBuidl = async (
+  req: NextApiRequest,
+  res: NextApiResponse
+) => {
+  if (!req.query.buidlId) {
+    return res.status(400).json({ message: "Missing buidlId" });
+  }
+
+  const buidlId = req.query.buidlId as string;
+
+  const proposals = await prisma.proposal.findMany({
+    where: {
+      buidlId: buidlId,
+    },
+  });
+
+  return res.status(200).json({ proposals });
+};
+
+const handleCreateProposal = async (
+  req: NextApiRequest,
+  res: NextApiResponse
+) => {
+  if (!req.body.buidlId) {
+    return res.status(400).json({ message: "Missing buidlId" });
+  }
+
+  if (!req.body.name) {
+    return res.status(400).json({ message: "Missing name" });
+  }
+
+  if (!req.body.purpose) {
+    return res.status(400).json({ message: "Missing purpose" });
+  }
+
+  if (!req.body.amount) {
+    return res.status(400).json({ message: "Missing amount" });
+  }
+
+  if (!req.body.endTimestamp) {
+    return res.status(400).json({ message: "Missing endTimestamp" });
+  }
+
+  if (!req.body.withdrawerAddresss) {
+    return res.status(400).json({ message: "Missing withdrawerAddress" });
+  }
+
+  const proposal = await prisma.proposal.create({
+    data: {
+      buidl: {
+        connect: {
+          id: req.body.buidlId,
+        },
+      },
+      name: req.body.name,
+      purpose: req.body.purpose,
+      amount: req.body.amount,
+      endTimestamp: req.body.endTimestamp,
+      withdrawerAddress: req.body.withdrawerAddress,
+    },
+  });
+
+  return res.status(200).json({ proposal });
+};
+
+export default handler;
---
+++ pages/api/updates/[index].ts
@@ -0,0 +1,74 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { prisma } from "../../../src/lib/db";
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  switch (req.method) {
+    case "GET":
+      await handleGetUpdatesForBuidl(req, res);
+      break;
+
+    case "POST":
+      await handleCreateUpdate(req, res);
+      break;
+
+    default:
+      return res.status(405).json({ message: "Method not allowed" });
+  }
+};
+
+const handleGetUpdatesForBuidl = async (
+  req: NextApiRequest,
+  res: NextApiResponse
+) => {
+  if (!req.query.buidlId) {
+    return res.status(400).json({ message: "Missing buidlId" });
+  }
+
+  const buidlId = req.query.buidlId as string;
+
+  const updates = await prisma.update.findMany({
+    where: {
+      buidlId: buidlId,
+    },
+  });
+
+  return res.status(200).json({ updates });
+};
+
+const handleCreateUpdate = async (
+  req: NextApiRequest,
+  res: NextApiResponse
+) => {
+  if (!req.body.buidlId) {
+    return res.status(400).json({ message: "Missing buidlId" });
+  }
+
+  if (!req.body.name) {
+    return res.status(400).json({ message: "Missing name" });
+  }
+
+  if (!req.body.description) {
+    return res.status(400).json({ messagwe: "Missing description" });
+  }
+
+  if (!req.body.updateNumber) {
+    return res.status(400).json({ messagwe: "Missing updateNumber" });
+  }
+
+  const update = await prisma.update.create({
+    data: {
+      buidl: {
+        connect: {
+          id: req.body.buidlId,
+        },
+      },
+      name: req.body.name,
+      description: req.body.description,
+      updateNumber: req.body.updateNumber,
+    },
+  });
+
+  return res.status(200).json({ update });
+};
+
+export default handler;
