e17716f332dfbce0edf3e93ba57b589827232e1f
Anish De <63192115+AnishDe12020@users.noreply.github.com>
2023-01-23 16:30:43 +0530

    again colorfix

--- src/theme/components/button.ts
+++ src/theme/components/button.ts
@@ -43,54 +43,11 @@ const accessibleColorMap: { [key: string]: AccessibleColor } = {
 const variantSolid = defineStyle((props) => {
   const { colorScheme: c } = props;
 
-  if (c === "brand") {
-    return {
-      color: "white",
-      bg: "brand.secondary",
-      _hover: { bg: "brand.tertiary" },
-      _active: { bg: "brand.quaternary" },
-    };
-  } else if (c === "accent") {
-    return {
-      color: "white",
-      bg: "accent.primary",
-      _hover: { bg: "accent.secondary" },
-      _active: { bg: "accent.tertiary" },
-    };
-  } else if (c === "gray") {
-    const bg = mode(`gray.100`, `whiteAlpha.200`)(props);
-
-    return {
-      bg,
-      _hover: {
-        bg: mode(`gray.200`, `whiteAlpha.300`)(props),
-        _disabled: {
-          bg,
-        },
-      },
-      _active: { bg: mode(`gray.300`, `whiteAlpha.400`)(props) },
-    };
-  }
-
-  const {
-    bg = `${c}.500`,
-    color = "white",
-    hoverBg = `${c}.600`,
-    activeBg = `${c}.700`,
-  } = accessibleColorMap[c] ?? {};
-
-  const background = mode(bg, `${c}.300`)(props);
-
   return {
-    bg: background,
-    color: mode(color, `gray.800`)(props),
-    _hover: {
-      bg: mode(hoverBg, `${c}.400`)(props),
-      _disabled: {
-        bg: background,
-      },
-    },
-    _active: { bg: mode(activeBg, `${c}.500`)(props) },
+    color: "white",
+    bg: "brand.secondary",
+    _hover: { bg: "brand.tertiary" },
+    _active: { bg: "brand.quaternary" },
   };
 });
 
